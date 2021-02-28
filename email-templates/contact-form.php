1fb5c4876b80080acded000d0e29d15ace6d6832
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-02-28 11:14:39 +0100

    added google analytics

--- email-templates/contact-form.php
+++ email-templates/contact-form.php
@@ -21,6 +21,17 @@ if( ! empty( $_POST['email'] ) ) {
 	$message = '
 		<html>
 		<head>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-187747465-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-187747465-3');
+</script>
+
 		<title>HTML email</title>
 		</head>
 		<body>
